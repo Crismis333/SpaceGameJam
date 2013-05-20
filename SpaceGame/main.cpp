156d8a3eca9bdee668b6d3186cadd79b59f65f1b
Jesper Sørensen <jeppester1@gmail.com>
2013-05-21 00:43:16 +0200

    More linux compatibility changes

--- SpaceGame/main.cpp
+++ SpaceGame/main.cpp
@@ -1,14 +1,13 @@
 #ifdef OS_WINDOWS
     #include "SDL.h"
 #else
-    #include "SDL/SDL.h"
+    #include <SDL/SDL.h>
 #endif
 
 int main( int argc, char* args[] )
 {
     //Start SDL
     SDL_Init( SDL_INIT_EVERYTHING );
-
 
 
     //Quit SDL
