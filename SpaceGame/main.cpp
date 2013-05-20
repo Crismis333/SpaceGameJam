6d23461c23abf79527fced8e4790de37a4a17c27
Jesper Sørensen <jeppester1@gmail.com>
2013-05-21 00:04:01 +0200

    SDL include fixed for linux

--- SpaceGame/main.cpp
+++ SpaceGame/main.cpp
@@ -1,14 +1,18 @@
-#include "SDL.h"
+#ifdef OS_WINDOWS
+    #include "SDL.h"
+#else
+    #include "SDL/SDL.h"
+#endif
 
 int main( int argc, char* args[] )
 {
     //Start SDL
     SDL_Init( SDL_INIT_EVERYTHING );
 
-	
-    
+
+
     //Quit SDL
     SDL_Quit();
-    
-    return 0;    
+
+    return 0;
 }
